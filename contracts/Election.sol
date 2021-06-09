5fbc557936bbc32440ba2828c576a8cd0ea49984
pablo <treendaa@gmail.com>
2021-06-09 14:49:32 +0200

    Deleted default contract, created Election contract with tests

---
+++ contracts/Election.sol
@@ -0,0 +1,64 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.3;
+
+import "hardhat/console.sol";
+
+contract Election {
+    struct Candidate {
+        uint256 id;
+        string name;
+        string desc;
+        string work;
+        string motto;
+        uint32 voteCount;
+    }
+    event votedEvent(uint32 indexed _candidateId);
+    mapping(address => bool) public voters;
+    mapping(uint32 => Candidate) public candidates;
+    uint32 public candidatesCount;
+
+    constructor() {
+        addCandidate(
+            "Donald Trump",
+            "A working man",
+            "President",
+            "Make America Great Again!"
+        );
+        addCandidate("Joe Biden", "Not sure", "Dunno", "Dunno");
+    }
+
+    function addCandidate(
+        string memory _name,
+        string memory _desc,
+        string memory _work,
+        string memory _motto
+    ) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(
+            candidatesCount,
+            _name,
+            _desc,
+            _work,
+            _motto,
+            0
+        );
+    }
+
+    function vote(uint32 _candidateId) public {
+        require(!voters[msg.sender], "We already counted your vote");
+        // require a valid candidate
+        require(
+            _candidateId > 0 && _candidateId <= candidatesCount,
+            "Candidate not found"
+        );
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
+}
---
+++ test/election-test.js
@@ -0,0 +1,58 @@
+const { expect } = require('chai');
+
+describe('Election', function () {
+	let Election, election, accounts;
+
+	beforeEach(async () => {
+		Election = await ethers.getContractFactory('Election');
+		election = await Election.deploy();
+		accounts = await ethers.getSigners();
+	});
+
+	it('Should initialize two candidates', async function () {
+		expect(await election.candidatesCount()).to.equal(2);
+	});
+	it('Should initialize with correct values', async function () {
+		const candidate = await election.candidates(1);
+		expect(candidate.name).to.equal('Donald Trump');
+		expect(candidate.desc).to.equal('A working man');
+		expect(candidate.work).to.equal('President');
+		expect(candidate.motto).to.equal('Make America Great Again!');
+
+		const candidate2 = await election.candidates(2);
+		expect(candidate2.name).to.equal('Joe Biden');
+		expect(candidate2.desc).to.equal('Not sure');
+		expect(candidate2.work).to.equal('Dunno');
+		expect(candidate2.motto).to.equal('Dunno');
+	});
+	it('Should throw an exception when candidate does not exist', async function () {
+		try {
+			await election.vote(0, {
+				from: accounts[0].address,
+			});
+		} catch (err) {
+			console.log(err.message);
+			expect(err.message).to.include('revert Candidate not found');
+		}
+	});
+
+	it('Should throw exception for double voting', async function () {
+		try {
+			await election.vote(1, {
+				from: accounts[0].address,
+			});
+			await election.vote(1, {
+				from: accounts[0].address,
+			});
+		} catch (err) {
+			console.log(err.message);
+			expect(err.message).to.include('revert We already counted your vote');
+		}
+	});
+	it('Should cast vote through voteEvent', async function () {
+		await election.vote(1, { from: accounts[0].address });
+		election.on('votedEvent', (e) => {
+			expect(e).to.be.equal(1);
+		});
+	});
+});
